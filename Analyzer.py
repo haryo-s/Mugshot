2d67bf6218b160c5906a13e008eb6daf57540f74
Fragrag <haryo.sukmawanto@gmail.com>
2019-05-13 22:48:33 +0200

    Fixed bug where an incorrect image halted the Analyzer

--- Analyzer.py
+++ Analyzer.py
@@ -26,15 +26,17 @@ def get_face_encoding(img_loc):
 
     :return: face location as numpy array and face encoding as list
     """
+
     try:
         image = face_recognition.load_image_file(img_loc)
+
         face_loc = face_recognition.face_locations(image)
         face_enc = face_recognition.face_encodings(image, face_loc)
         return face_loc, face_enc
         
-    except Exception as error:
-        print("Couldn't load image")
-        return None, None
+    except OSError as e:
+        return [], []
+        
 
 def analyze_folder(folder_location, dest_file):
     data = {}
@@ -45,15 +47,14 @@ def analyze_folder(folder_location, dest_file):
         with open(json_file) as json_string:
             json_data = json.load(json_string)
             # print(json_data)
-            try:
-                image_file = json_data['entries']['image']
-            except Exception as error:
-                pass
+            image_file = json_data['entries']['image']
+            print("Information for " + image_file + " loaded")
 
-            if image_file != None:
+            if image_file != None or os.stat(folder_location + image_file).st_size > 0:
                 file_face_loc, file_face_enc = get_face_encoding(folder_location + image_file) 
-
+                print(image_file + " loaded!")
                 if len(file_face_loc) == 1 and len(json_data['entries']['charges']) != 0:
+                    print("Processing " + image_file)
                     print(nr)
                     # file_face_enclist = file_face_enc.tolist()
                     data['mugshots'].append({
@@ -68,7 +69,7 @@ def analyze_folder(folder_location, dest_file):
                     print('Entry skipped! No charges found!')
                     
             else:
-                print('Entry skipped! Image not found!')
+                print('Entry skipped! Image not found or is illegible!')
                 
 
 
